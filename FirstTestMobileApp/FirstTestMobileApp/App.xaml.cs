a20fda34a3c95f934388b5bdc0f52217f6fcae3f
Brikka23 <ilyabogdanov787@gmail.com>
2022-09-17 03:38:38 +0300

    Добавьте файлы проекта.

---
+++ FirstTestMobileApp/FirstTestMobileApp/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirstTestMobileApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new FirstTestMobileApp.Page1();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ FirstTestMobileApp/FirstTestMobileApp/Page1.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirstTestMobileApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        int cout;
+        public Page1()
+        {
+            InitializeComponent();
+
+        }
+        
+        private void ButtonEntry1(object sender,EventArgs e)
+        {
+            Label1.Text = Entry1.Text;
+        }
+        private void ButtonClick1(object sender, EventArgs e)
+        {
+            cout++;
+            string coutext = Convert.ToString(cout);
+            Score.Text = coutext;
+            if (cout == 50)
+            {
+                Label labelHide1 = new Label
+                {   Text = "Have a nice day :)", 
+                    FontSize = 30,
+                    VerticalOptions = LayoutOptions.Center, 
+                    HorizontalOptions = LayoutOptions.Center ,
+                    TextColor = Color.FromHex("#00FF00") 
+                };
+                main.Children.Add(labelHide1);
+            }
+
+        }
+
+    }
+}
